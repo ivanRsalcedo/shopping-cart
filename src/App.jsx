534a9fa93155a7fa6a9ce9d2f7427a34202b4125
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-08-12 13:14:20 -0400

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 
-function Banger(props) {
-  return <h1 style={{ fontSize: props.fontSize }}>{props.text}</h1>
-}
-
 function App() {
-  const [fontSize, setFontSize] = useState(100)
 
   return (
-    <main style={{ textAlign: 'center', marginTop: 250 }}>
-
-      <button onClick={() => setFontSize(f => f + 10)}>
-        +
-      </button>
-      <Banger text="are you grateful?" fontSize={fontSize} />
-    </main>
+    <>
+      <header>
+        <nav>
+          
+        </nav>
+      </header>
+    </>
   )
 }
 
