fc8f5a7dae8bfd0968d236bb5588c06956a92db2
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-08-12 18:22:32 -0400

    Base styles and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router'
+import { ShoppingCart } from 'lucide-react'
+import Home from './components/Home'
+import Shop from './components/Shop'
+import Cart from './components/Cart'
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <header>
-        <nav>
-          
+        <nav className='nav-links'>
+          <Link to='/'>Home</Link>
+          <Link to='/shop'>Shop</Link>
+          <Link to="/cart">Cart</Link>
         </nav>
       </header>
-    </>
+      
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </main>
+
+      <footer>
+        <p>made by ivan</p>
+      </footer>
+    </BrowserRouter>
   )
 }
 
