de66a17cecd223ece769f64f477a5fc82e386296
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-08-12 19:12:41 -0400

    Home page styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import './App.mobile.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router'
-import { ShoppingCart } from 'lucide-react'
 import Home from './components/Home'
 import Shop from './components/Shop'
 import Cart from './components/Cart'
+import Pikachu from './assets/pikachu.svg'
 
 function App() {
 
@@ -16,13 +17,14 @@ function App() {
           <Link to="/cart">Cart</Link>
         </nav>
       </header>
-      
+
       <main>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/shop' element={<Shop />} />
           <Route path='/cart' element={<Cart />} />
         </Routes>
+        <img src={Pikachu} alt="Pikachu silhouette" className="pikachu-bg" />
       </main>
 
       <footer>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router'
+
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to PokéMart Online!</p>
+
+            <button className="hero-button">
+                <Link to="/shop">
+                    <h2>Start Your Trainer Journey!</h2>
+                </Link>
+            </button>
+        </div>
     )
 }
